213c0028bd800175bf994b79b7cd5edc3dcd3a35
Kastor438 <Kastor438@hotmail.com>
2023-01-06 13:20:28 -0400

    Setting up layout for upcoming page.

--- src/Components/Upcoming.js
+++ src/Components/Upcoming.js
@@ -1,4 +1,6 @@
 import '../StyleSheets/Upcoming.css';
+import 'react-calendar/dist/Calendar.css';
+
 import React, { useEffect, useRef, useState } from 'react';
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
@@ -7,15 +9,39 @@ import { getDatabase, ref, set, remove, child, get, onValue } from "firebase/dat
 import Calendar from 'react-calendar';
 
 const Upcoming = () => {
+  const [calendarDateHeader, SetCalendarDateHeader] = useState();
   const [calendarDate, SetCalendarDate] = useState(new Date());
 
+  useEffect(() => {
+    console.log(calendarDate);
+    var calendarDateElement = React.createElement('h3', {id : 'selectedDateHeader'}, calendarDate.getDate() + '/' + (calendarDate.getMonth() + 1) + '/' + calendarDate.getFullYear());
+    SetCalendarDateHeader(calendarDateElement);
+  }, [calendarDate]);
   return (
     <div id='upcomingDiv'>
       <div id='calendarDiv'>
         <Calendar id='upcomingCalendar' onChange={SetCalendarDate} value={calendarDate} />
       </div>
       <div id='eventDiv'>
-        <span>p</span>
+        {calendarDateHeader}
+        <div id='eventInfoDiv'>
+          <div id='eventDetailsDiv'>
+            <div id='eventLiveAtDiv'>
+              <label id='eventLiveAtLabel' className='eventInfoLabel'>Live at: </label>
+              <span>Kai Brady's</span>
+            </div>
+            <div id='eventStartTimeDiv'>
+              <label id='eventStartTimeLabel' className='eventInfoLabel'>Start Time: </label>
+              <span>9pm</span>
+            </div>
+            <div id='eventDescriptionDiv'>
+              <span id='eventDescription'>Wing Night!!!!!!!!!!!</span>
+            </div>
+          </div>
+          <div id='eventImageDiv'>
+            <img src='#' alt='An image representing the event'/>
+          </div>
+        </div>
       </div>
     </div>
   );
