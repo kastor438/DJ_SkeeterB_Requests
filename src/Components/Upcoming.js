74acf1bcb084f2911bf2387c39d504a034c288f1
Justin MacKinnon <kastor438@hotmail.com>
2023-01-06 11:57:04 -0400

    Fixed auto authentication glitch. Working on upcoming page.

--- src/Components/Upcoming.js
+++ src/Components/Upcoming.js
@@ -1,7 +1,23 @@
+import '../StyleSheets/Upcoming.css';
+import React, { useEffect, useRef, useState } from 'react';
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getAuth } from "firebase/auth";
+import { getDatabase, ref, set, remove, child, get, onValue } from "firebase/database";
+import Calendar from 'react-calendar';
+
 const Upcoming = () => {
+  const [calendarDate, SetCalendarDate] = useState(new Date());
+
   return (
-      <div id='upcomingDiv'>
+    <div id='upcomingDiv'>
+      <div id='calendarDiv'>
+        <Calendar id='upcomingCalendar' onChange={SetCalendarDate} value={calendarDate} />
       </div>
+      <div id='eventDiv'>
+        <span>p</span>
+      </div>
+    </div>
   );
 }
 
