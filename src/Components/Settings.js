8fc304554b725b2f45958d25d4876f368a86d905
Justin MacKinnon <kastor438@hotmail.com>
2023-02-15 01:53:35 -0400

    Started setting up new Settings page. Requests export now shows requestor by user ID.

--- src/Components/Settings.js
+++ src/Components/Settings.js
@@ -2,7 +2,9 @@ import '../StyleSheets/Settings.css';
 
 import React, { useEffect, useRef, useState } from 'react';
 import { initializeApp } from "firebase/app";
-import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
+import { getAuth } from "firebase/auth";
+import { getDatabase, ref, child, get, set } from "firebase/database";
+import { getDownloadURL, getStorage, ref as storageRef, uploadBytes, listAll } from "firebase/storage";
 import { Navigate, Link } from 'react-router-dom';
 
 const firebaseConfig = {
@@ -18,11 +20,92 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
+const storage = getStorage(app);
 
 const Settings = props => {
+  const [navigateToHome, SetNavigateToHome] = useState(false);
+
+  // DOM Refs
+  const logoPreviewImageRef = useRef();
+  
+  // DB Refs
+  const db = getDatabase();
+  const dbRef = ref(getDatabase());
+
+  useEffect(() => {
+    if(!auth.currentUser || (auth.currentUser.uid !== 'GXoCbNpX6lPq3hYxRvIrfvUXMsx1' && auth.currentUser.uid !== 'bExKDb4uJTbis2GZOL8fm6clrw83')){
+      SetNavigateToHome(true);
+    }
+  }, [props.authUser])
+  
+  if(navigateToHome === true){
+    return <Navigate to='/'/>;
+  }
   return (
     <div id='settingsDiv'>
-      
+      <div>
+        <div id='settingsHeaderBar'>
+          <div className='settingsHeaderButtonDiv'>
+            <button>Navigation Bar</button>
+          </div>
+          <div className='settingsHeaderButtonDiv'>
+            <button>Theme</button>
+          </div>
+          <div className='settingsHeaderButtonDiv'>
+            <button>Venues</button>
+          </div>
+          <div className='settingsHeaderButtonDiv'>
+            <button>Socials</button>
+          </div>
+          <div className='settingsHeaderButtonDiv'>
+            <button>Security</button>
+          </div>
+        </div>
+        <div>
+          <div id='navbarSettingsDiv' className='settingsSection'>
+            <div>
+              <h3>Navigation Display</h3>
+            </div>
+            <div>
+              <div id='logoSettingsDiv'>
+                <div id='logoPreviewDiv'>
+                  <img id='logoPreviewImage' ref={logoPreviewImageRef} src='/skeeterB-Logo.ico' alt=''/>
+                </div>
+                <div>
+                  <label>Navigation Bar Logo: </label>
+                  <input type='file' id=''/>
+                </div>
+              </div>
+              <div id='navbarHeaderSettingsDiv'>
+                <div>
+                  <label>Navigation Header: </label>
+                  <input type='text' id=''/>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div id='themeSettingsDiv' className='settingsSection'>
+            <div>
+              <h3>Themes</h3>
+            </div>
+          </div>
+          <div id='venueSettingsDiv' className='settingsSection'>
+            <div>
+              <h3>Venues</h3>
+            </div>
+          </div>
+          <div id='socialsSettingsDiv' className='settingsSection'>
+            <div>
+              <h3>Socials</h3>
+            </div>
+          </div>
+          <div id='securitySettingsDiv' className='settingsSection'>
+            <div>
+              <h3>Security</h3>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
