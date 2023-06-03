6d578a2fb5c43791a7e39e73acace8844a83c97e
Kastor438 <Kastor438@hotmail.com>
2023-06-03 03:50:55 -0300

    Added socials on menu panel.

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -300,6 +300,44 @@ const NavBar = props => {
             <li className='navBarLink'><NavLink ref={upcomingLinkRef} to='/Upcoming' className={({isActive}) => isActive ? 'activeLink' : ''} onClick={(e) => ToggleMenuPanel(e.target)}>Upcoming</NavLink></li>
             {skeeterSpecificLinks}
           </ul>
+          <div id='skeeterSocialsDiv'>
+            <div id='skeeterTwitterDivContainer' className='skeeterSocialDivContainer'>
+              <a className='skeeterSocialsLink' target='blank' href='https://twitter.com/SkeeterB1'>
+                <div id='skeeterTwitterLinkDiv' className='skeeterSocialLinkDiv'>
+                  <div id='twitterLinkImageDiv' className='skeeterSocialImageDiv'>
+                    <img id='twitterLinkImage' className='skeeterSocialImage' src='./TwitterLogo.png'/>
+                  </div>
+                  <div id='twitterLinkTextDiv' className='socialLinkTextDiv'>
+                    <h4 id='twitterLinkText' className='socialLinkText'>Twitter</h4>
+                  </div>
+                </div>
+              </a>
+            </div>
+            <div id='skeeterInstagramDivContainer' className='skeeterSocialDivContainer'>
+              <a className='skeeterSocialsLink' target='blank' href='https://www.instagram.com/djskeeterb/'>
+              <div id='skeeterInstagramLinkDiv' className='skeeterSocialLinkDiv'>
+                <div id='instagramLinkImageDiv' className='skeeterSocialImageDiv'>
+                  <img id='instagramLinkImage' className='skeeterSocialImage' src='./InstagramLogo.png'/>
+                </div>
+                <div id='instagramLinkTextDiv' className='socialLinkTextDiv'>
+                  <h4 id='instagramLinkText' className='socialLinkText'>Instagram</h4>
+                </div>
+              </div>
+              </a>
+            </div>
+            <div id='skeeterMixCloudDivContainer' className='skeeterSocialDivContainer'>
+              <a className='skeeterSocialsLink' target='blank' href='https://www.mixcloud.com/djskeeterb/'>
+              <div id='skeeterMixCloudLinkDiv' className='skeeterSocialLinkDiv'>
+                <div id='mixCloudLinkImageDiv' className='skeeterSocialImageDiv'>
+                  <img id='mixCloudLinkImage' className='skeeterSocialImage' src='./MixCloudLogo.png'/>
+                </div>
+                <div id='mixCloudLinkTextDiv' className='socialLinkTextDiv'>
+                  <h4 id='mixCloudLinkText' className='socialLinkText'>MixCloud</h4>
+                </div>
+              </div>
+              </a>
+            </div>
+          </div>
         </div>
       </nav>
     </div>
